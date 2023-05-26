853fd8824bc465da4b191da7e5c843cfd007bcd1
Andrei Dodu <andrei.dodu@hotmail.it>
2023-05-27 00:05:57 +0200

    feature: job picture resizing

--- src/main/java/com/andreidodu/util/ImageUtil.java
+++ src/main/java/com/andreidodu/util/ImageUtil.java
@@ -1,11 +1,12 @@
 package com.andreidodu.util;
 
 import com.andreidodu.constants.ApplicationConst;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.imgscalr.Scalr;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -13,6 +14,8 @@ import java.util.Base64;
 
 public class ImageUtil {
 
+
+    public static final String IMAGE_FORMAT_NAME = "png";
 
     public static byte[] convertBase64StringToBytes(final String base64String) throws UnsupportedEncodingException {
         final String dataSegment = base64String.substring(base64String.indexOf(",") + 1);
@@ -23,8 +26,7 @@ public class ImageUtil {
     public static String calculateFileName(final String seed, final String base64ImageFull, final byte[] imageBytesData) throws NoSuchAlgorithmException, IOException {
         final byte[] signedImageBytesData = createNewArrayWithBytesAtTheEnd(imageBytesData, seed.getBytes());
         final String bytesHashString = calculateBytesHashString(signedImageBytesData);
-        final String fileExtension = calculateFileExtension(base64ImageFull);
-        return bytesHashString + "." + fileExtension;
+        return bytesHashString + ".png";
     }
 
     public static byte[] createNewArrayWithBytesAtTheEnd(final byte[] target, final byte[] bytesToBeAdded) throws IOException {
@@ -48,5 +50,25 @@ public class ImageUtil {
         FileOutputStream outputStream = new FileOutputStream(fullFilePath);
         outputStream.write(data);
         outputStream.close();
+    }
+
+    public static byte[] resizeImage(byte[] originalImage, int targetWidth) {
+        try {
+            InputStream is = new ByteArrayInputStream(originalImage);
+            BufferedImage newBi = ImageIO.read(is);
+            BufferedImage bufferedImage = Scalr.resize(newBi, targetWidth);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, IMAGE_FORMAT_NAME, byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception e) {
+            return originalImage;
+        }
+    }
+
+
+    public static ImmutablePair<Integer, Integer> retrieveImageSize(byte[] originalImage) throws IOException {
+        InputStream is = new ByteArrayInputStream(originalImage);
+        BufferedImage newBi = ImageIO.read(is);
+        return new ImmutablePair<>(newBi.getWidth(), newBi.getHeight());
     }
 }
